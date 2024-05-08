6ae3dd446b90658772bdd31b6bd973fa63b5cbd8
Vishal Bisht <131914809+Vishal-Bisht@users.noreply.github.com>
2024-05-08 12:40:25 +0530

    Update Inheritance.java

--- Lab-Update/Inheritance.java
+++ Lab-Update/Inheritance.java
@@ -4,16 +4,24 @@ class Base {
     }
 }
 
-class Derived extends Base {
+class Derived1 extends Base {
      void n() {
         System.out.println("This is child class");
     }
 }
 
+class Derived2 extends Derived1 {
+    void p(){
+        System.out.println("This is another child class");
+    }
+}
+
 public class Inheritance {
     public static void main(String[] args) {
-        Derived d = new Derived();
-        d.m();
-        d.n(); 
+        Derived1 d1 = new Derived1();
+        Derived2 d2 = new Derived2();
+        d1.m();
+        d1.n();
+        d2.p();
     }
 }

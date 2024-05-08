5f6ebd46f1740943b6ffcb2563772acad1df6544
mherenow <venkatesh09072003@gmail.com>
2024-05-08 12:07:36 +0530

    getId instead of threadId

--- Lab-Update/MultiThreading.java
+++ Lab-Update/MultiThreading.java
@@ -1,6 +1,6 @@
 class Threads extends Thread {
     public void run() {
-        System.out.println("Thread " + Thread.currentThread().threadId() + " is running.");
+        System.out.println("Thread " + Thread.currentThread().getId() + " is running.");
     }
 }
 
